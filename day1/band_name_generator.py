6b03f36620f8cf1a982fefc7a43c4abd3b05b5fa
Rehan <skmdrehan231@gmail.com>
2025-12-06 16:58:11 +0530

    day1

---
+++ day1/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to Ban name generator!!")
+city_name = input("Enter your City Name? ")
+pet_name = input("Enter your Pet Name? ")
+
+band_name = city_name+" "+pet_name
+
+print(band_name)
